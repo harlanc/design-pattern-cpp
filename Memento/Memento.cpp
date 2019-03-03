0b99ea75ebf567dadf1b0970534c9326b967208e
HarlanC <wawacry@qq.com>
2019-03-03 11:26:48 +0800

    memento

---
+++ Memento/Memento.cpp
@@ -0,0 +1,77 @@
+#include <string>
+#include <iostream>
+#include <memory>
+#include <vector>
+
+class Memento{
+    public:
+    Memento(const std::string state){
+        _state = state;
+    }
+
+    std::string getState(){
+        return _state;
+    }
+
+    private:
+    std::string _state;
+
+};
+
+class Originator{
+    public:
+    void setState(const std::string state){
+        _state = state;
+    }
+
+    std::string getState(){
+        return _state;
+    }
+
+    std::shared_ptr<Memento> saveStateToMemento(){
+        return std::make_shared<Memento>(_state);
+    }
+    
+    void getStateFromMemento( std::shared_ptr<Memento> memento){
+        _state = memento->getState();
+    }
+    private:
+    std::string _state;
+};
+
+class CareTaker{
+    public:
+    void add (const std::shared_ptr<Memento> &state){
+        _mementos.push_back(state);
+    }
+    std::shared_ptr<Memento> get(int idx){
+        return _mementos[idx];
+    }
+    private:
+    std::vector<std::shared_ptr<Memento> > _mementos;
+};
+
+
+int main(){
+
+    std::shared_ptr<Originator> originator =  std::make_shared<Originator>();
+    std::shared_ptr<CareTaker> caretaker =  std::make_shared<CareTaker>(); 
+
+    originator->setState("State #1");
+    originator->setState("State #2");
+    caretaker->add(originator->saveStateToMemento());
+    originator->setState("State #3");
+    caretaker->add(originator->saveStateToMemento());
+
+    originator->setState("State #4");
+    std::cout<<"Current State: "<<originator->getState()<<std::endl;
+    originator->getStateFromMemento(caretaker->get(0));
+    std::cout<<"First saved State: "<<originator->getState()<<std::endl;
+    originator->getStateFromMemento(caretaker->get(1));
+    std::cout<<"Second saved State: "<<originator->getState()<<std::endl;
+
+    
+    return 0;
+}
+
+
